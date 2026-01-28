c7a473401058106cf31f6331a5f28b0bf4cc63f2
asbjorn <asbjorn.salhus@gmail.com>
2026-01-28 18:48:27 +0100

    added some project links with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import chessShader from './assets/chess_shader.png'
+import octreeShader from './assets/octree_shader.png'
+import truchetShader from './assets/truchet_shader.png'
+import asteroidsShader from './assets/asteroids_shader.png'
+import voxelsGithub from './assets/voxels_github.jpg'
+import githubLogo from '/github.svg'
 import './App.css'
+
+function Shadertoy() {
+  return <>
+    <div>
+      <a href="https://www.shadertoy.com/user/abje" id="shadertoy">Shadertoy</a>
+      <br></br>
+      <a href="https://www.shadertoy.com/view/4d3BDM">
+        <img src={chessShader} className="shader"></img>
+      </a>
+      <a href="https://www.shadertoy.com/view/4sVfWw">
+        <img src={octreeShader} className="shader"></img>
+      </a>
+      <br></br>
+      <a href="https://www.shadertoy.com/view/MtcBDM">
+        <img src={truchetShader} className="shader"></img>
+      </a>
+      <a href="https://www.shadertoy.com/view/wdjSzz">
+        <img src={asteroidsShader} className="shader"></img>
+      </a>
+    </div>
+  </>
+}
+
+
+
+function GithubProjects() {
+  return <>
+    <div>
+      <a href="https://github.com/abjeni">
+        <img src={githubLogo} id="github" className="logo" alt="Github logo" />
+      </a>
+      <br></br>
+      <a href="https://abjeni.github.io/test-voxel/">
+        <img src={voxelsGithub} className="githubIcon"></img>
+      </a>
+      <a href="https://abjeni.github.io/test-voxel/">
+        repository link
+      </a>
+    </div>
+  </>
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <GithubProjects/>
+    <Shadertoy/>
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  </>
 }
 
 export default App
